c399e77056f3a44cec128cf03de8eee51f566b7d
qwestomir99-crypto <qwestomir99@gmail.com>
2026-05-15 04:35:18 +0300

    Update app.py
    Hbv

--- opt/render/project/src/app.py
+++ opt/render/project/src/app.py
@@ -1,10 +1,5 @@
-import os
-import time
 import threading
-import requests
-import Flask
-from flask
-
+from flask import Flask
 from ping_main import ping_main_bot
 
 app = Flask(__name__)
@@ -14,7 +9,5 @@ def health():
     return "Pinger is alive", 200
 
 if __name__ == "__main__":
-    # Запускаем активный пинг в фоновом потоке
     threading.Thread(target=ping_main_bot, daemon=True).start()
-    # Запускаем Flask-сервер для Render
     app.run(host='0.0.0.0', port=10000)
